ad9d1b504aee4df25cf30b6f268ed75c96848a3a
florianmahner <florian.mahner@icloud.com>
2022-12-14 21:41:34 +0100

    Move Readme to docs

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name="toml_argparse",
+    version="0.0.1",
+    author="Florian P. Mahner",
+    author_email="florian.mahner@gmail.com",
+    license="LICENSE",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
